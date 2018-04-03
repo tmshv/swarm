d6b9c08c5fcfcf80d746e7a42191cb29f4274110
Roman Timashev <roman@tmshv.ru>
2018-04-03 19:47:16 +0300

    RandomWalkerBehaviour refactoring

--- src/swarm/behaviours/RandomWalkBehaviour.js
+++ src/swarm/behaviours/RandomWalkBehaviour.js
@@ -3,12 +3,9 @@ import Vector from '../Vector'
 
 export default class RandomWalkBehaviour extends Behaviour {
     run() {
-        const v = this.accelerate
-        const target = Vector.add(this.agent.location, new Vector(
-            -v / 2 + Math.random() * v,
-            -v / 2 + Math.random() * v,
-        ))
+        const angle = Math.random() * Math.PI * 2
+        const force = Vector.fromAngle(angle)
 
-        this.seekAccelerated(target)
+        this.forceAccelerated(force)
     }
 }
