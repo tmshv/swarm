9d55e4c584c7e2f869366283ac52a1325e093d93
Roman Timashev <roman@tmshv.ru>
2018-04-03 19:46:44 +0300

    AgentsView update to not use ttl

--- src/swarm/views/AgentsView.js
+++ src/swarm/views/AgentsView.js
@@ -10,15 +10,13 @@ export default class AgentsView extends View {
         if (this.clear) this.draw.clear()
 
         const ctx = this.draw.context
+        ctx.fillStyle = `rgba(200, 0, 0, ${1})`
 
         const s = 1
         this.simulation
             .getAgents()
             .forEach(agent => {
-                let alpha = agent.ttl / 10000
-                ctx.fillStyle = `rgba(200, 0, 0, ${alpha})`
-
-                this.draw.rectCenter(agent.location, s, s)
+                this.draw.rect(agent.location, s, s)
             })
     }
 }
