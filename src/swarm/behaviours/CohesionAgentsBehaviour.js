03c99fbd4597430ed0925e05a63ed4912b3ff7ad
Roman Timashev <roman@tmshv.ru>
2018-04-05 21:44:21 +0300

    added CohesionAgentsBehaviour

---
+++ src/swarm/behaviours/CohesionAgentsBehaviour.js
@@ -0,0 +1,22 @@
+import MovingBehavior from './MovingBehavior'
+import Vector from '../Vector'
+
+export default class CohesionAgentsBehaviour extends MovingBehavior {
+    init({radius}) {
+        this.radius = radius
+
+        this.average = new Vector(0, 0)
+    }
+
+    run({agentsPool}) {
+        const agents = agentsPool.getInRadius(this.agent.location, this.radius)
+
+        this.average.set(0, 0)
+        for (let a of agents) {
+            this.average.add(a.location)
+        }
+        this.average.divide(agents.length)
+
+        this.seek(this.average)
+    }
+}
