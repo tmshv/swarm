f26d8330d8e7f563ef182616a7b3526d96177586
Roman Timashev <roman@tmshv.ru>
2018-04-05 21:44:51 +0300

    RandomWalkBehavior updated

--- src/swarm/behaviours/RandomWalkBehaviour.js
+++ src/swarm/behaviours/RandomWalkBehaviour.js
@@ -1,11 +1,9 @@
-import Behaviour from './Behaviour'
+import MovingBehavior from './MovingBehavior'
 import Vector from '../Vector'
 
-export default class RandomWalkBehaviour extends Behaviour {
+export default class RandomWalkBehaviour extends MovingBehavior {
     run() {
         const angle = Math.random() * Math.PI * 2
-        const force = Vector.fromAngle(angle)
-
-        this.forceAccelerated(force)
+        this.force(Vector.fromAngle(angle))
     }
 }
