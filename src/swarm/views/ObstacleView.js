54c25335b440b7c910314fc46e789649fe292630
Roman Timashev <roman@tmshv.ru>
2018-04-09 03:24:24 +0300

    ObstacleView updated

--- src/swarm/views/ObstacleView.js
+++ src/swarm/views/ObstacleView.js
@@ -4,15 +4,16 @@ export default class ObstacleView extends SimulationView {
     render() {
         const ctx = this.draw.context
 
-        // const b = Math.floor(Math.random() * 256)
-        // ctx.fillStyle = `rgba(200, 0, ${b}, 1)`
-        // this.draw.screenRect(0, 0, 50, 50)
-
         const alpha = 1
         ctx.strokeStyle = `rgba(200, 200, 200, ${alpha})`
+        ctx.fillStyle = `rgba(250, 250, 250, 1)`
 
         this.simulation.env.obstacles
             .forEach(obstacle => {
+                const coords = obstacle.lines.map(x => x.a)
+                this.draw.path(coords)
+                ctx.fill()
+
                 obstacle.lines.forEach(line => {
                     this.draw.line(line)
 
@@ -23,7 +24,6 @@ export default class ObstacleView extends SimulationView {
                         .mult(2)
                     this.draw.vector(c, n)
                 })
-                // this.draw.plus(obstacle.centroid, 5)
             })
     }
 }
