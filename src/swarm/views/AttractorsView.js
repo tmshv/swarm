21d3093a8b83d9d934dcb80aead0b3cd8376586f
Roman Timashev <roman@tmshv.ru>
2018-04-15 20:36:02 +0300

    AttractorsView render emitter association

--- src/swarm/views/AttractorsView.js
+++ src/swarm/views/AttractorsView.js
@@ -8,6 +8,8 @@ export default class AttractorsView extends EnvironmentView {
         const ctx = this.draw.context
 
         this.environment.attractors.forEach(a => {
+            this.renderEmitterAssociation(a)
+
             const location = a.location
             const agentsPoolSize = a.agents.length
             const alpha = agentsPoolSize
@@ -23,5 +25,16 @@ export default class AttractorsView extends EnvironmentView {
                 this.draw.text(location, `${agentsPoolSize}`, textOffset)
             }
         })
+    }
+
+    renderEmitterAssociation(attractor){
+        const ctx = this.draw.context
+
+        ctx.strokeStyle = `rgba(0, 0, 0, 0.1)`
+        ctx.lineWidth = 1
+        attractor.getAssociatedEmitters()
+            .forEach(e => {
+                this.draw.path([attractor.location, e.location])
+            })
     }
 }
