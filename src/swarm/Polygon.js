854836a36b42d119d95a776ae8e38c06beb4b686
Roman Timashev <roman@tmshv.ru>
2018-05-09 22:09:57 +0300

    Polygon::getCentroid

--- src/swarm/Polygon.js
+++ src/swarm/Polygon.js
@@ -1,4 +1,5 @@
 import SwarmObject from './SwarmObject'
+import {getCentroid} from './lib/geometry'
 
 export default class Polygon extends SwarmObject {
     get coords() {
@@ -8,5 +9,9 @@ export default class Polygon extends SwarmObject {
     constructor({coords}) {
         super()
         this._coords = coords
+    }
+
+    getCentroid() {
+        return getCentroid(this._coords)
     }
 }
