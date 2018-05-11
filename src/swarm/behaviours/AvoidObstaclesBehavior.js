9888a95492e5907638cd98ccd2ac89bc3f7fa56e
Roman Timashev <roman@tmshv.ru>
2018-05-11 04:11:37 +0300

    AvoidObstaclesBehavior improved

--- src/swarm/behaviours/AvoidObstaclesBehavior.js
+++ src/swarm/behaviours/AvoidObstaclesBehavior.js
@@ -1,4 +1,5 @@
 import MovingBehavior from './MovingBehavior'
+import Line from '../Line'
 import Vector from '../Vector'
 
 export default class AvoidObstaclesBehavior extends MovingBehavior {
@@ -13,42 +14,39 @@ export default class AvoidObstaclesBehavior extends MovingBehavior {
     run({environment}) {
         if (!this.lines) this.updateLines(environment)
 
-        this.reflection = null
+        this.impactForce = null
+        this.intersection = null
         this.edge = null
+
+        const location = this.agent.location
+        // const obstacle = environment.findObstacle(location, this.radius)
+        // this.obstacle = obstacle
+        // if (!obstacle) return
 
         const predict = this.predictLocation()
         const predictDirection = this.getPredictionVector()
-        const location = this.agent.location
 
+        // let edge = this.getNearestEdge(obstacle, location, predictDirection)
         let edge = this.getNearestEdge(location, predictDirection)
         if (!edge) return
 
-        if (edge.distSquared(predict) >= this.predictionDistanceSquared) {
-            return
-        }
+        // const moveLine = new Line(location, predict)
+        // this.intersection = edge.getLineIntersection(moveLine)
+        this.intersection = edge.project(location)
 
         let force = this.getForce(edge)
-        force.add(this.getNormalForce(edge))
+        // let normalForce = this.getNormalForce(edge)
+        // force.add(normalForce)
 
-        this.force(force)
         this.agent.acceleration.direct(force)
 
         this.edge = edge
-        this.reflection = force
-    }
-
-    getForce(edge) {
-        const force = edge.getDirection()
-        const predictDirection = this.getPredictionVector()
-
-        if (force.dot(predictDirection) < 0) {
-            force.reverse()
-        }
-
-        return force
+        this.impactForce = force
     }
 
     getNormalForce(edge) {
+        const predict = this.predictLocation()
+        // const n = edge.distSquared(predict) - this.predictionDistance
         const n = this.predictionDistance
         return edge
             .normal
@@ -56,11 +54,34 @@ export default class AvoidObstaclesBehavior extends MovingBehavior {
             .setLength(n)
     }
 
+    getForce(edge) {
+        const futureLocation = this.predictLocation()
+        const distSquared = edge.distSquared(futureLocation)
+        const n = Math.sqrt(
+            this.predictionDistanceSquared - distSquared
+        )
+
+        const normal = edge.normal
+            .clone()
+            .setLength(n)
+
+        const force = this
+            .getPredictionVector()
+            .add(normal)
+        // const force = edge.getDirection()
+        // const predictDirection = this.getPredictionVector()
+        //
+        // if (force.dot(predictDirection) < 0) {
+        //     force.reverse()
+        // }
+
+        return force
+    }
+
     getPredictionVector() {
         return this.agent.velocity
             .clone()
-            .normalize()
-            .mult(this.predictionDistance)
+            .setLength(this.predictionDistance)
     }
 
     predictLocation() {
@@ -74,16 +95,45 @@ export default class AvoidObstaclesBehavior extends MovingBehavior {
 
         const nextCoord = Vector.add(coord, direction)
 
-        for (let line of this.lines) {
-            if (line.normal.dot(direction) < 0) { // edge in opposite direction
-                const coordProjected = line.project(coord)
-                if (line.isBetween(coordProjected)) {
-                    // const nextCoordProjected = line.project(nextCoord)
+        const moveLine = new Line(coord, nextCoord)
 
-                    // if (nextCoordProjected)
+        // const lines = obstacle.lines
+        const lines = this.lines
+        for (let line of lines) {
+            if (line.distSquared(nextCoord) < this.predictionDistanceSquared) {
+                if (line.normal.dot(direction) < 0) { // edge in opposite direction
+                    // edges.push([line, line.distSquared(coord)])
 
-                    edges.push([line, line.distSquared(nextCoord)])
+                    const project = line.project(coord)
+                    if (project) {
+                        if (line.isBetween(project)) {
+                            edges.push([line, coord.distSquared(project)])
+                        }
+                    }
                 }
+                continue
+
+                const intersection = line.getLineIntersection(moveLine)
+                if (intersection) {
+                    const intersectionDirection = Vector.sub(intersection, coord)
+                    if (direction.dot(intersectionDirection) < 0) { // intersection in opposite direction
+
+                    } else {
+                        if (line.isBetween(intersection)) {
+                            edges.push([line, coord.distSquared(intersection)])
+                        }
+                    }
+                }
+
+                // const coordProjected = line.project(coord)
+                // if (line.isBetween(coordProjected)) {
+                //     // const nextCoordProjected = line.project(nextCoord)
+                //
+                //     // if (nextCoordProjected)
+                //
+                //     edges.push([line, line.distSquared(nextCoord)])
+                // }
+                // }
             }
         }
 
@@ -91,6 +141,20 @@ export default class AvoidObstaclesBehavior extends MovingBehavior {
             return a[1] - b[1]
         })
 
+        // const ps = this.lines
+        //     .map(line => {
+        //         const projection = line.project(coord)
+        //
+        //         const between = line.isBetween(projection)
+        //         const distSquared = Vector
+        //             .sub(coord, projection)
+        //             .lengthSquared
+        //         return [line, distSquared, between]
+        //     })
+        //     .filter(x => x[2])
+        //     .sort((a, b) => {
+        //         return a[1] - b[1]
+        //     })
         return edges.length
             ? edges[0][0]
             : null
