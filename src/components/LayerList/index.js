1fcc1d8ec8e08f8fe794c5e5307d06a3d7a4fc6c
Roman Timashev <roman@tmshv.ru>
2018-07-08 23:25:49 +0300

    LayerList cleanup

--- src/components/LayerList/index.js
+++ src/components/LayerList/index.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import './styles.less'
 
 function LayerList({ items, onChange }) {
-
     return (
         <ul className={'LayerList'}>
             {items.map((x, i) => (
