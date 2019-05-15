b0bd128378502f37abeebe24e01532cf314657cf
Roman Timashev <roman@tmshv.ru>
2019-05-15 18:50:35 +0300

    Dat component item update

--- src/components/Dat/index.jsx
+++ src/components/Dat/index.jsx
@@ -4,22 +4,51 @@ import React, { Component } from 'react';
 import DatGui, { DatBoolean, DatColor, DatNumber, DatString } from 'react-dat-gui';
 
 export default class Dat extends Component {
-    renderItem = (x, i) => {
-        {/* <DatString path='package' label='Package' />
-                <DatNumber path='power' label='Power' min={9000} max={9999} step={1} />
-                <DatBoolean path='isAwesome' label='Awesome?' />
-                <DatColor path='feelsLike' label='Feels Like' /> */}
-        
-        return (
-            <DatNumber
-                key={i}
-                path={x.field}
-                label={x.label}
-                min={x.min}
-                max={x.max}
-                step={x.step}
-            />
-        )
+    renderItem = (x, i) => {      
+        switch (x.type) {
+            case 'number': {
+                return (
+                    <DatNumber
+                        key={i}
+                        path={x.field}
+                        label={x.label}
+                        min={x.min}
+                        max={x.max}
+                        step={x.step}
+                    />
+                )
+            }
+                
+            case 'color': {
+                return (
+                    <DatColor
+                        key={i}
+                        path={x.field}
+                        label={x.label}
+                    />
+                )
+            }
+            
+            case 'boolean': {
+                return (
+                    <DatBoolean
+                        key={i}
+                        path={x.field}
+                        label={x.label}
+                    />
+                )
+            }
+
+            case 'string': {
+                return (
+                    <DatString
+                        key={i}
+                        path={x.field}
+                        label={x.label}
+                    />
+                )
+            }
+        }
     }
 
     render() {
