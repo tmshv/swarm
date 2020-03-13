dcf88d0f1dcf18583e591a7c9d366042c529ccff
Roman Timashev <roman@tmshv.ru>
2020-03-13 18:16:51 +0300

    move createAgent factory to separate file

--- src/app/init.ts
+++ src/app/init.ts
@@ -1,33 +1,16 @@
 import Simulation from '../swarm/Simulation'
 import AgentPool from '../swarm/AgentPool'
-import Agent from '../swarm/Agent'
 import Environment from '../swarm/Environment'
 import Attractor from '../swarm/Attractor'
 import Id from '../swarm/Id'
 import Emitter from '../swarm/Emitter'
 import Vector from '../swarm/Vector'
-import SeekNearestAttractorBehavior from '../swarm/behaviors/SeekNearestAttractorBehavior'
 import Pheromones from '../swarm/Pheromones'
-import Obstacle from '../swarm/Obstacle'
-import AvoidObstaclesBehavior from '../swarm/behaviors/AvoidObstaclesBehavior'
-import TtlBehavior from '../swarm/behaviors/TtlBehavior'
-import ComposableBehavior from '../swarm/behaviors/ComposableBehavior'
-import LimitAccelerationBehavior from '../swarm/behaviors/LimitAccelerationBehavior'
-import SeekLocationBehavior from '../swarm/behaviors/SeekLocationBehavior'
-import AgentBehavior from '../swarm/AgentBehavior'
-import Line from '../swarm/Line'
 import Tag from '../swarm/Tag'
 import ObstacleType from '../swarm/ObstacleType'
-import AvoidPointObstaclesBehavior from '../swarm/behaviors/AvoidPointObstaclesBehavior'
-import PointObstacle from '../swarm/PointObstacle'
 import PathObstacle from '../swarm/PathObstacle'
-import BoidBehavior from '../swarm/behaviors/BoidBehavior'
 import AttractorType from '../swarm/AttractorType'
-import AlignAgentsBehavior from '../swarm/behaviors/AlignAgentsBehavior'
-import SeparateAgentsBehavior from '../swarm/behaviors/SeparateAgentsBehavior'
-import CohesionAgentsBehavior from '../swarm/behaviors/CohesionAgentsBehavior'
-import RandomWalkBehavior from '../swarm/behaviors/RandomWalkBehavior'
-import SpreadPheromonesBehavior from '../swarm/behaviors/SpreadPheromonesBehavior'
+import { createAgent } from './createAgent'
 
 export type SwarmUserData = {
     [name: string]: any
@@ -179,116 +162,6 @@ export function getCameraCenter(data: SwarmData): Vector {
     }
 
     return createGeometry(target.geometry)
-}
-
-function createAgent(loc, vars) {
-    const initialVelocity = new Vector(0, 0)
-
-    let behavior = null
-
-    if (!behavior) {
-        behavior = ComposableBehavior.compose(
-            new TtlBehavior({
-                ttl: vars.agentTtl,
-            }),
-
-            // BOID
-
-            // new AlignAgentsBehavior({
-            //     accelerate: .21,
-            //     radius: 250,
-            // }),
-
-            new SeparateAgentsBehavior({
-                accelerate: 0.2,
-                radius: 250,
-            }),
-
-            // new CohesionAgentsBehavior({
-            //     accelerate: .5,
-            //     radius: 250,
-            // }),
-
-            // OTHER
-
-            // new ConditionalBehavior({
-            //     predicate: new IfTargetReachedBehavior({
-            //         minDistance: 5,
-            //     }),
-            //     trueBranch: AgentBehavior.SEEK_LOCATION,
-            //     falseBranch: new RandomWalkBehavior({
-            //         accelerate: 0.25,
-            //     }),
-            // }),
-
-            // new WalkToNearestAttractorBehavior({}),
-
-            new SeekNearestAttractorBehavior({
-                accelerate: 1,
-                thresholdDistQuad: 10000 ** 2,
-                attractorTypes: [AttractorType.UNKNOWN],
-            }),
-
-            // new InteractAgentsBehavior({
-            //     accelerate: 0.4,
-            //     radius: 25,
-            //     initialInterest: 200,
-            // }),
-
-            // new RandomWalkBehavior({
-            //     accelerate: 0.01,
-            // }),
-
-            // new InteractEnvironmentBehavior({
-            //     accelerate: 0.1
-            // }),
-
-            // new InteractPheromonesBehavior({
-            //     accelerate: .05,
-            // }),
-
-            new SpreadPheromonesBehavior({
-                // pheromones,
-                pheromonesName: 'kek',
-            }),
-
-            // AVOID OBSTACLE
-
-            // new AvoidPointObstaclesBehavior({
-            //     accelerate: 0.1,
-            //     predictionDistance: 50,
-            //     radius: 50,
-            // }),
-
-            new AvoidObstaclesBehavior({
-                accelerate: 1,
-                predictionDistance: 10,
-                radius: 50,
-            }),
-
-            // OTHER
-
-            new LimitAccelerationBehavior({
-                limit: 1,
-            })
-        )
-    }
-
-    const noise = Vector.fromAngle(Math.random() * Math.PI * 2)
-    noise.mult(0.1)
-
-    const a = new Agent({
-        behavior,
-    })
-    a.damp = 0.75
-    a.location.setFrom(Vector.add(loc, noise))
-    a.velocity.setFrom(initialVelocity)
-    a.addBehavior(AgentBehavior.SEEK_LOCATION, new SeekLocationBehavior({
-        accelerate: 0.1,
-        threshold: 2,
-    }))
-
-    return a
 }
 
 function createGeometry(geom) {
