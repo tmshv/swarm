0ca2d8b97b0b05fcc8edd9ef7008f26bd2fffed7
Roman Timashev <roman@tmshv.ru>
2020-08-20 12:46:03 +0300

    swarm Camera -> ts

--- src/swarm/render/Camera.ts
+++ src/swarm/render/Camera.ts
@@ -1,14 +1,17 @@
 import Vector from '../Vector'
-import {getWindowHeight, getWindowWidth} from '../lib/browser'
+import { getWindowHeight, getWindowWidth } from '../lib/browser'
 
 export default class Camera {
-    constructor(window) {
+    private _window: Window
+    private location: Vector
+
+    constructor(window: Window) {
         this._window = window
         this.location = new Vector(0, 0)
     }
 
-    getScreenCenterOffset() {
-        const m = window.devicePixelRatio
+    public getScreenCenterOffset() {
+        const m = this._window.devicePixelRatio
 
         return new Vector(
             getWindowWidth(this._window) / 2 * m,
