9331e4b9315d7edee77b73254ebc59beac0f6806
Roman Timashev <roman@tmshv.ru>
2023-09-06 00:06:42 +0300

    next config esm

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
-module.exports = {
-    env: {
-    },
-    webpack(config) {
-        config.resolve.modules.push(__dirname)
-        return config;
-    },
-}
+export const env = {};
+// export function webpack(config, options) {
+//     // shader import support
+//     config.module.rules.push({
+//         test: /\.glsl$/,
+//         use: [
+//             // {
+//             //     loader: "emit-file-loader",
+//             //     options: {
+//             //         name: "dist/[path][name].[ext]",
+//             //     },
+//             // },
+//             // "babel-loader",
+//             "webpack-glsl-loader",
+//         ],
+//     });
+//
+//     return config;
+// }
