912cb1c213092055aa0b0eef775b9ce765910b58
Roman Timashev <roman@tmshv.ru>
2023-09-06 16:13:51 +0300

    crafting swarm package

--- packages/swarm/src/index.ts
+++ packages/swarm/src/index.ts
@@ -1,1 +1,50 @@
-export * from "./swarm"
+import Agent from "@/swarm/Agent"
+import Simulation from '@/swarm/Simulation'
+import AgentPool from '@/swarm/AgentPool'
+import Environment from '@/swarm/Environment'
+import Attractor from '@/swarm/Attractor'
+import Id from '@/swarm/Id'
+import Emitter from '@/swarm/Emitter'
+import Vector from '@/swarm/Vector'
+import Pheromones from '@/swarm/Pheromones'
+import ObstacleType from '@/swarm/ObstacleType'
+import Tag from '@/swarm/Tag'
+import PathObstacle from '@/swarm/PathObstacle'
+import AttractorType from '@/swarm/AttractorType'
+import ComposableBehavior from '@/swarm/behaviors/ComposableBehavior'
+import TtlBehavior from '@/swarm/behaviors/TtlBehavior'
+import SeekLocationBehavior from '@/swarm/behaviors/SeekLocationBehavior'
+import SeekNearestAttractorBehavior from '@/swarm/behaviors/SeekNearestAttractorBehavior'
+import SeparateAgentsBehavior from '@/swarm/behaviors/SeparateAgentsBehavior'
+import SpreadPheromonesBehavior from '@/swarm/behaviors/SpreadPheromonesBehavior'
+import AvoidObstaclesBehavior from '@/swarm/behaviors/AvoidObstaclesBehavior'
+import LimitAccelerationBehavior from '@/swarm/behaviors/LimitAccelerationBehavior'
+import InteractPheromonesBehavior from '@/swarm/behaviors/InteractPheromonesBehavior'
+import AgentBehavior from '@/swarm/AgentBehavior'
+
+export {
+    Agent,
+    Simulation,
+    AgentPool,
+    Environment,
+    Attractor,
+    Id,
+    Emitter,
+    Vector,
+    Pheromones,
+    ObstacleType,
+    Tag,
+    PathObstacle,
+    AttractorType,
+    ComposableBehavior,
+    TtlBehavior,
+    SeekLocationBehavior,
+    SeekNearestAttractorBehavior,
+    SeparateAgentsBehavior,
+    SpreadPheromonesBehavior,
+    AvoidObstaclesBehavior,
+    LimitAccelerationBehavior,
+    InteractPheromonesBehavior,
+    AgentBehavior,
+}
+
