c471479045bdcf71611cb1edeef69ac64b7acaad
Roman Timashev <roman@tmshv.ru>
2023-09-09 16:48:21 +0300

    build updates

--- packages/swarm/rollup.config.js
+++ packages/swarm/rollup.config.js
@@ -1,12 +1,25 @@
 import typescript from '@rollup/plugin-typescript'
+import { dts } from 'rollup-plugin-dts'
 
-export default {
-    input: 'src/index.ts',
-    output: {
-        dir: 'dist',
-        format: 'es'
+export default [
+    {
+        input: 'build/index.js',
+        output: {
+            dir: 'dist',
+            format: 'es',
+        },
+        // plugins: [
+        //     typescript(),
+        // ]
     },
-    plugins: [
-        typescript()
-    ]
-};
+    // {
+    //     input: ' build/index.d.ts',
+    //     output: {
+    //         file: 'dist/index.t.ts',
+    //         format: 'es',
+    //     },
+    //     plugins: [
+    //         dts(),
+    //     ]
+    // },
+]
