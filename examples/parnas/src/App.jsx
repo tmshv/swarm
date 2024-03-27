f535c045850337d06eeab76dfb33343a33c0ad8a
Roman Timashev <roman@tmshv.ru>
2024-03-28 00:29:17 +0300

    parnas example updated

--- examples/parnas/src/App.jsx
+++ examples/parnas/src/App.jsx
@@ -1,8 +1,9 @@
 import { useSwarm } from '@tmshv/swarm-react'
 import { SwarmViewport } from '@tmshv/swarm-react'
+import { createControls, getLayers } from './init'
 
 const App = () => {
-    const controller = useSwarm("/parnas.js", null)
+    const controller = useSwarm("/parnas.js", null, getLayers, createControls)
     if (!controller) {
         return (
             <div>loading</div>
