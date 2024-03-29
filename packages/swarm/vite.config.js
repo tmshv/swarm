27a3bc93dc9d6fb46ca148866659945cefbbab9f
Roman Timashev <roman@tmshv.ru>
2024-03-29 18:03:19 +0300

    add sourcemap

--- packages/swarm/vite.config.js
+++ packages/swarm/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         }),
     ],
     build: {
+        sourcemap: true,
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'Swarm',
